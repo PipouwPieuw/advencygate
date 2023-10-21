fa08a96bc7b0270696174d28e7b3129371762dfb
Pierre <pierre@advency.fr>
2023-10-21 16:43:50 +0200

    Maj AA / BB / CC

---
+++ Avenue Azur/script.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+	var mik = String.fromCharCode;
+	var azd = [
+		mik(116, 104, 101, 114, 101, 115, 97, 109, 97, 121),
+		mik(112, 114, 111, 106, 101, 99, 116, 105, 108, 101),
+		mik(108, 97, 115, 118, 101, 103, 97, 115),
+		mik(117, 114, 97, 110, 117, 115),
+		mik(118, 111, 114, 119, 101, 114, 107),
+		mik(114, 101, 103, 100, 97, 114),
+		mik(112, 101, 117, 103, 101, 111, 116),
+		mik(103, 114, 97, 110, 100, 114, 111, 110, 100),
+		mik(109, 97, 110, 103, 97, 114, 101, 118, 97),
+		mik(104, 97, 110, 100, 112, 108, 97, 110, 116),
+		mik(97, 108, 111, 99, 97, 115, 105, 97),
+		mik(115, 97, 101, 110, 99, 104, 97, 105),
+		mik(115, 105, 103, 114, 117, 110),
+		mik(121, 111, 117, 114, 105, 100, 106, 111, 114, 107, 97, 101, 102, 102),
+		mik(109, 105, 110, 105),
+		mik(109, 111, 114, 100, 111, 114),
+		mik(101, 100, 100, 105, 101),
+		mik(115, 111, 108, 100, 111, 109, 105)
+	];
+	var pok = [
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false,
+		false
+	];
+	$('.js-input').on('keyup', function() {
+		var index = parseInt($(this).attr('data-index')) - 1;
+		var value = $(this).val().toLowerCase().replaceAll(' ', '');
+		if(value == azd[index]) {
+			$(this).addClass('solved');
+			$(this).blur();
+			pok[index] = true;
+			checkAnswers();
+		}
+	});
+	function checkAnswers() {
+		for(erg in pok)
+			if(!pok[erg])
+				return;
+		var v = String.fromCharCode(103, 114, 97, 110, 100, 114, 111, 110, 100);
+		var timeOffsetInput = 0;
+		var isEven = true;
+		$('.js-input').each(function() {
+			var that = $(this);
+			if(isEven) {
+				isEven = false;
+				timeOffsetInput += 100;
+			}
+			else {
+				isEven = true;
+			}
+			setTimeout(function() {
+				that.addClass('move');
+			}, timeOffsetInput);
+		});
+		var timeOffsetAnswer = 100;		
+		$('.js-list-answers').addClass('visible');
+		$('.js-answer').each(function() {
+			timeOffsetAnswer += 100;
+			var that = $(this);
+			var index = that.attr('data-answer');
+			$(this).text(v.charAt(index));
+			setTimeout(function() {					
+				that.addClass('visible');
+			}, timeOffsetAnswer);
+		});
+	}
+});
---
+++ Chemin Citron/script.js
@@ -0,0 +1,140 @@
+
+$(document).ready(function() {
+	var activeItem = "";
+	$('.js-timeline-item').click(function() {
+		var elem = $(this);
+		activeItem = elem;
+		var overlay = $('.js-overlay');
+		var overlayImg = $('.js-overlay-image');
+		var overlayDesc = $('.js-overlay-desc');
+		var imgSrc = elem.find('.js-img').attr('src');
+		var imgDate = elem.find('.js-date').text();
+		var imgDesc = imgDate + ' - ' + elem.find('.js-desc').text();
+		overlayImg.attr('src', imgSrc);
+		overlayDesc.text(imgDesc);
+		overlay.addClass('active');
+	});
+	$('.js-overlay').click(function() {
+		$(this).removeClass('active');
+		activeItem = "";
+	});
+	$('.js-icon-picker').click(function() {
+		$('.js-timeline-icon.active').removeClass('active');
+		$(this).removeClass('active');
+	});
+	$('.js-timeline-icon').click(function() {
+		$(this).addClass('active');
+		$('.js-icon-picker').addClass('active');
+	});
+	$('.js-icon-trigger').click(function() {
+		var icon = $(this).attr('data-icon');
+		$('.js-timeline-icon.active').attr('data-icon', icon);
+		checkAnswers();
+	});
+	$('.js-overlay-button').click(function(e) {
+		e.stopPropagation();
+		var dir = $(this).attr('data-btn');
+		var nextItem = "";
+		if(dir == "prev") {
+			if(activeItem.is(':first-of-type')) {
+				var itemParent = activeItem.parent();
+				if(itemParent.is(':first-of-type')) {
+					nextItem = $('.list_timeline ul:last-of-type li:last-of-type');
+				}
+				else {
+					nextItem = itemParent.prev().prev().find('li:last-of-type');
+				}
+			}
+			else {
+				nextItem = activeItem.prev();
+			}
+		}
+		else {
+			if(activeItem.is(':last-of-type')) {
+				var itemParent = activeItem.parent();
+				if(itemParent.is(':last-of-type')) {
+					nextItem = $('.list_timeline ul:first-of-type li:first-of-type');
+				}
+				else {
+					nextItem = itemParent.next().next().find('li:first-of-type');
+				}
+			}
+			else {
+				nextItem = activeItem.next();
+			}
+		}
+		var overlayImg = $('.js-overlay-image');
+		var overlayDesc = $('.js-overlay-desc');
+		var imgSrc = nextItem.find('.js-img').attr('src');
+		var imgDate = nextItem.find('.js-date').text();
+		var imgDesc = imgDate + ' - ' + nextItem.find('.js-desc').text();
+		overlayImg.attr('src', imgSrc);
+		overlayDesc.text(imgDesc);
+		activeItem = nextItem;
+	});
+	function checkAnswers() {
+		var v = String.fromCharCode(48, 55, 49, 52, 48, 49, 49, 53, 49, 56, 49, 48, 49, 54, 48, 51, 48, 57, 48, 52, 49, 55, 49, 50, 48, 54, 49, 51, 48, 53, 48, 56, 48, 50, 49, 49);
+		var str = '';
+		$('.js-timeline-icon').each(function() {
+			str += $(this).attr('data-icon');
+		});
+		if(str == v) {
+			showAll();
+		}
+	}
+	function showAll() {
+		var counter = 0;
+		var dates = [
+			'08/05/16',
+			'29/07/17',
+			'30/01/18',
+			'01/06/18',
+			'07/07/18',
+			'27/07/18',
+			'26/10/18',
+			'02/03/19',
+			'20/05/19',
+			'20/07/19',
+			'06/09/19',
+			'17/10/19',
+			'09/10/20',
+			'19/07/21',
+			'16/06/21',
+			'22/07/22',
+			'31/03/23',
+			'14/04/23',
+		];
+		var titles = [
+			'Première Ruée des Fadas',
+			'Séminaire au Puy du Ouf',
+			'Test live de l\'application TYM',
+			'Séminaire à San Sebastian',
+			'Travaux au bureau des Carmes',
+			'Emménagement au bureau des Carmes',
+			'5 ans de Advency au Tranchoir',
+			'Week-end en Aveyron chez Jules',
+			'Séminaire à Edimbourgh',
+			'Enterrement de vie de garçon de Fabien à Berlin',
+			'Mariage de Fabien et Nadège',
+			'Les 6 ans de Advency',
+			'Séminaire à Donges',
+			'Semaine de rush Taakaro',
+			'Séminaire à Londres',
+			'Pot de départ de Florent',
+			'Pot de départ de Ludovic',
+			'Repas chez Walid',
+		];
+		$('.js-timeline-noimg').each(function() {
+			counter += 1;
+			var zero = counter >= 10 ? "" : "0";			
+			var newElem = $('<li class="list_timeline__item js-timeline-item"></li>');
+			var newElemImg = $('<div class="list_timeline__image"></div>');
+			newElemImg.append('<img class="list_timeline__img js-img" src="assets/img/coquelicot/' + zero + counter + 'bassine.png"/>');
+			newElem.append(newElemImg);
+			newElem.append('<span class="list_timeline__date js-date">' + dates[counter-1] + '</span>');
+			newElem.append('<span class="list_timeline__desc js-desc">' + titles[counter-1] + '</span>');
+			newElem.insertAfter($(this));
+			$(this).remove();
+		});
+	}
+});
