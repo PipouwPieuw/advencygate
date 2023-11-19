0cca1218222849bad2340e67788a24c0a91cbed4
Pierre <pierre@advency.fr>
2023-11-19 10:55:30 +0100

    VV mobile

--- voie_verte/script.js
+++ voie_verte/script.js
@@ -21,90 +21,117 @@ $(document).ready(function() {
 	var soundSwap = new Audio('assets/swap.wav');
 	var dong = String.fromCharCode;
 	
-    $('.card_box:not(.fixed)').draggable({
-        zIndex: 2,
-        revert: false,
-        revertDuration: 200,
-        start: function(event, ui) {
-            ui.helper.data('dropped', false);
-            startPosition = $(this).position();
-            $('.card_box').css({ 'z-index': 0 });
-            $(this).css({ 'z-index': 1 });
-        },
-        stop: function(event, ui) {
-            var draggable = $(this);
-            if (!ui.helper.data('dropped')) {
+	if($(window).width() > 1199) {
+	    $('.card_box:not(.fixed)').draggable({
+	        zIndex: 2,
+	        revert: false,
+	        revertDuration: 200,
+	        start: function(event, ui) {
+	            ui.helper.data('dropped', false);
+	            startPosition = $(this).position();
+	            $('.card_box').css({ 'z-index': 0 });
+	            $(this).css({ 'z-index': 1 });
+	        },
+	        stop: function(event, ui) {
+	            var draggable = $(this);
+	            if (!ui.helper.data('dropped')) {
+					soundSwap.pause();
+					soundSwap.currentTime = 0;
+					soundSwap.play();
+	                moveElem(draggable);
+	            } else {
+	                draggable.css({ 'z-index': 0 });
+	            }
+	        }
+	    });
+
+	    $('.card_container').droppable({
+	        accept: $('.card_box'),
+	        tolerance: "pointer",
+	        drop: function(event, ui) {
+	            ui.helper.data('dropped', true);
 				soundSwap.pause();
 				soundSwap.currentTime = 0;
 				soundSwap.play();
-                moveElem(draggable);
-            } else {
-                draggable.css({ 'z-index': 0 });
-            }
-        }
-    });
+	            var draggable = ui.draggable;
+	            var droppable = $(this);
+	            var dropPos = droppable.position();
+	            var dataPos = droppable.attr('data-pos');
+		        var elem = $('.card_box[data-pos=' + dataPos + ']');
+	            if (elem.length > 0
+	             && elem.attr('data-val') != draggable.attr('data-val')) {
+	             	if(draggable.hasClass('placed')) {
+	             		var dragPos = draggable.attr('data-pos');
+	             		var originPos = $('.card_container[data-pos=' + dragPos + ']').position();
+			            elem.css({ 'z-index': 3, 'pointer-events': 'none' }).animate({
+			                left: originPos.left,
+			                top: originPos.top,
+			            }, 200).attr('data-pos', dragPos).addClass('placed');
+				        setTimeout(function() {
+				            elem.css({ 'pointer-events': 'all' });
+				        }, 200);
+	    	        }
+	    	        else {
+	    	            moveElem(elem);    	        	
+	    	        }
+	            }
+	            draggable.css({ 'z-index': 3, 'pointer-events': 'none' }).animate({
+	                left: dropPos.left,
+	                top: dropPos.top,
+	            }, 200).attr('data-pos', dataPos).addClass('placed');
+		        checkValues();
+	            setTimeout(function() {
+	                draggable.css({ 'pointer-events': 'all' });
+	                droppable.css({ 'pointer-events': 'all' });
+	            }, 200);
+	        }
+	    });
 
-    $('.card_container').droppable({
-        accept: $('.card_box'),
-        tolerance: "pointer",
-        drop: function(event, ui) {
-            ui.helper.data('dropped', true);
-			soundSwap.pause();
-			soundSwap.currentTime = 0;
-			soundSwap.play();
-            var draggable = ui.draggable;
-            var droppable = $(this);
-            var dropPos = droppable.position();
-            var dataPos = droppable.attr('data-pos');
-	        var elem = $('.card_box[data-pos=' + dataPos + ']');
-            if (elem.length > 0
-             && elem.attr('data-val') != draggable.attr('data-val')) {
-             	if(draggable.hasClass('placed')) {
-             		var dragPos = draggable.attr('data-pos');
-             		var originPos = $('.card_container[data-pos=' + dragPos + ']').position();
-		            elem.css({ 'z-index': 3, 'pointer-events': 'none' }).animate({
-		                left: originPos.left,
-		                top: originPos.top,
-		            }, 200).attr('data-pos', dragPos).addClass('placed');
-			        setTimeout(function() {
-			            elem.css({ 'pointer-events': 'all' });
-			        }, 200);
-    	        }
-    	        else {
-    	            moveElem(elem);    	        	
-    	        }
-            }
-            draggable.css({ 'z-index': 3, 'pointer-events': 'none' }).animate({
-                left: dropPos.left,
-                top: dropPos.top,
-            }, 200).attr('data-pos', dataPos).addClass('placed');
-	        checkValues();
-            setTimeout(function() {
-                draggable.css({ 'pointer-events': 'all' });
-                droppable.css({ 'pointer-events': 'all' });
-            }, 200);
-        }
-    });
-
-    function moveElem(elem) {
-        elem.css({ 'z-index': 3, 'pointer-events': 'none' }).animate({
-            left: elem.attr('data-left'),
-            top: elem.attr('data-top'),
-        }, 200).attr('data-pos', "").removeClass('placed');
-        setTimeout(function() {
-            elem.css({ 'pointer-events': 'all' });
-        }, 200);
-    }
+	    function moveElem(elem) {
+	        elem.css({ 'z-index': 3, 'pointer-events': 'none' }).animate({
+	            left: elem.attr('data-left'),
+	            top: elem.attr('data-top'),
+	        }, 200).attr('data-pos', "").removeClass('placed');
+	        setTimeout(function() {
+	            elem.css({ 'pointer-events': 'all' });
+	        }, 200);
+	    }
+	}
+	else {
+		var clickedSlot;
+		$('.card_container').on('click', function() {
+			clickedSlot = $(this);
+			$('.mobile_cards').addClass('visible');
+		});
+		$('.mobile_cards__item').on('click', function() {
+			var icon = $(this).attr('data-val');
+			clickedSlot.attr('data-val', icon);
+			clickedSlot.find('i').remove();
+			$('<i class="webfont webfont-' + icon + '"></i>').appendTo(clickedSlot);
+			$('.mobile_cards').removeClass('visible');
+			setChosenCards();
+			checkValues();
+		});
+	}
 
 	function checkValues() {
 		var validString = '030813151118060114161702040910051207';
 		var currentString = '';
 		$('.card_container').each(function() {
-			var pos = $(this).attr('data-pos');
-			if(!$('.card_box[data-pos="' + pos + '"]').length > 0)
-				return false;
-			else
-				currentString += $('.card_box[data-pos="' + pos + '"]').attr('data-val');
+			if($(window).width() > 1199) {
+				var pos = $(this).attr('data-pos');
+				if(!$('.card_box[data-pos="' + pos + '"]').length > 0)
+					return false;
+				else
+					currentString += $('.card_box[data-pos="' + pos + '"]').attr('data-val');
+			}
+			else {
+				var attr = $(this).attr('data-val');
+				if(typeof attr !== 'undefined' && attr !== false)
+					currentString += $(this).attr('data-val');
+				else
+					return false;
+			}
 		});
 		if(currentString == validString) {
 			setTimeout(function() {
@@ -127,8 +154,28 @@ $(document).ready(function() {
 				soundSwap.currentTime = 0;
 				soundSwap.play();
 				$('.card_box[data-pos=' + zero + i + ']').addClass('green');
-				moveElem($('.card_box[data-pos=' + zero + i + ']'));
+				if($(window).width() > 1199) {
+					moveElem($('.card_box[data-pos=' + zero + i + ']'));
+				}
+				else {
+					$('.card_container[data-pos=' + zero + i + '] i').remove();
+					$('.card_container[data-pos=' + zero + i + ']').removeAttr('data-val');
+				}
 		    }, i * 150);
 		}
+	}
+
+	function setChosenCards() {
+		var vals = [];
+		$('.card_container').each(function() {
+			vals.push($(this).attr('data-val'));
+		});
+		$('.mobile_cards__item').each(function() {
+			var val = $(this).attr('data-val');
+			if(vals.includes(val))
+				$(this).addClass('chosen');
+			else
+				$(this).removeClass('chosen');
+		});
 	}
 });
