9c9fc91e3b93b256b1994d946cc3c95323ddcc55
Pierre <pierre@advency.fr>
2023-11-19 17:11:01 +0100

    Minified scripts and styles

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-$(document).ready(function() {
-	
-	$('.js-route').on('click', function(e) {
-		$('html').addClass('ovh');
-		$('body').addClass('no_click');
-		$(this).addClass('triggered');
-		if($(this).hasClass('list_levels__bg')) {
-			$(this).parent().addClass('triggered');
-			$('.list_levels').addClass('triggered');
-		}
-		var url = $(this).attr('data-url');
-		setTimeout(function() {
-			location.href = url;
-		}, 500);
-	});
-});
-
-
+$(document).ready(function(){$(".js-route").on("click",function(s){$("html").addClass("ovh"),$("body").addClass("no_click"),$(this).addClass("triggered"),$(this).hasClass("list_levels__bg")&&($(this).parent().addClass("triggered"),$(".list_levels").addClass("triggered"));var a=$(this).attr("data-url");setTimeout(function(){location.href=a},750)})});
